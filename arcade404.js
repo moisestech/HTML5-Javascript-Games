cb1929af9530b67212670514d00431c122a45e1f
Moises404 <sesmoi.moises@live.com>
2013-03-27 12:10:47 -0400

    changed css, will be adding tictactoegame

--- arcade404.js
+++ arcade404.js
@@ -1,3 +1,17 @@
+//disables scrolling
 document.body.scroll = "no";
 document.body.style.overflow = 'hidden';
 document.height = window.innerHeight;
+
+
+//Create blink function for working arcades
+var currentBlink = "images/arcades/arcade01_Working.png";
+
+var arcade01 = document.getElementById("arcade01");
+
+
+setInterval(function() {
+	arcade01.setAttribute("src", currentBlink);
+
+	currentBlink = currentBlink === "images/arcades/arcade01_Working.png" ? "images/arcades/arcade01.png": "images/arcades/arcade01_Working.png";
+}, 1000);
