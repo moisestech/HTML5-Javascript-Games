886652f1a570870fce58b6320ee5994e71d156ed
Moises404 <sesmoi.moises@live.com>
2013-04-02 23:34:52 -0400

    adding countDown feature

--- games/FizzBuzz/fizzBuzz.js
+++ games/FizzBuzz/fizzBuzz.js
@@ -1,7 +1,7 @@
 //global variables
 var numberPicked = prompt("pick a number to FizzBuzz!");
 
-setInterval( "alert('Hello')", 5000 );
+//setInterval( "alert('Hello')", 5000 );
 
 for (i=0; i <=numberPicked; i++) {
 
@@ -10,6 +10,28 @@ for (i=0; i <=numberPicked; i++) {
 	document.body.appendChild(currentNumber);
 
 
+
+//timer
+var timer;
+var totalSeconds;
+
+function createTimer(TimerID, Time) {
+	timer = document.getElementById(TimerID);
+	totalSeconds = time;
+
+	updateTimer();
+	window.setTimeout("tick()", 1000);
+}
+
+function tick() {
+	totalSeconds -= 1;
+	updateTimer();
+	window.setTimeout("tick()", 1000);
+}
+
+function updateTimer() {
+	timer.innerHTML = totalSeconds;
+}
 
 	/*if (i % 3 === 0 && i % 5 === 0) {
 		console.log("FizzBuzz!");
