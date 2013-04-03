269a5dda24aa2ec9b8f0eea9958feaeb996b3941
Moises404 <sesmoi.moises@live.com>
2013-04-02 23:26:48 -0400

    adding timer

--- games/FizzBuzz/fizzBuzz.js
+++ games/FizzBuzz/fizzBuzz.js
@@ -1,12 +1,15 @@
 //global variables
 var numberPicked = prompt("pick a number to FizzBuzz!");
 
+setInterval( "alert('Hello')", 5000 );
 
 for (i=0; i <=numberPicked; i++) {
 
 	var currentNumber = document.createElement("li");
 	currentNumber.innerHTML = i;
 	document.body.appendChild(currentNumber);
+
+
 
 	/*if (i % 3 === 0 && i % 5 === 0) {
 		console.log("FizzBuzz!");
