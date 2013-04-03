15f5fc46de282dc16da5e47ebacc93634f388ef3
Moises404 <sesmoi.moises@live.com>
2013-04-02 23:16:19 -0400

    beginning to change FizzBuzz to show all numbers

--- games/FizzBuzz/fizzBuzz.js
+++ games/FizzBuzz/fizzBuzz.js
@@ -1,15 +1,15 @@
 //global variables
-var numberPicked = 60;
+var numberPicked = prompt("pick a number to FizzBuzz!");
 
 
-for (i=0; i <numberPicked; i++) {
+for (i=0; i <=numberPicked; i++) {
 	if (i % 3 === 0 && i % 5 === 0) {
 		console.log("FizzBuzz!");
 	} else if (i % 3 === 0) {
 		console.log("Fizz");
 	} else if (i % 5 === 0) {
 		console.log("Buzz");
+	} else {
+		console.log(i);
 	}
 }
-
-console.log("works!")
