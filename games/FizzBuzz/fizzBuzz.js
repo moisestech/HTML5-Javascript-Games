122621bd4853d1e66131468f72e5371dc0d9109a
Moises404 <sesmoi.moises@live.com>
2013-04-02 23:44:52 -0400

    started adding fizz, buzz, and fizzBuzz buttons

--- games/FizzBuzz/fizzBuzz.js
+++ games/FizzBuzz/fizzBuzz.js
@@ -1,6 +1,34 @@
 //global variables
 var numberPicked = prompt("pick a number to FizzBuzz!");
 
+
+//create gameButtons
+var fizz = document.createElement("div");
+fizz.innerHTML = "fizz";
+fizz.addEventListener("click", fizzEvent, false);
+
+var buzz = document.createElement("div");
+buzz.innerHTML = "buzz";
+buzz.addEventListener("click", buzzEvent, false);
+
+var fizzBuzz = document.createElement("div");
+fizzBuzz.innerHTML = "fizzBuzz";
+fizzBuzz.addEventListener("click", fizzBuzzEvent, false);
+
+
+/* functions */
+var fizz = function () {
+	console.log("fizz!");
+}
+
+var buzz = function () {
+	console.log("buzz!");
+}
+
+var fizzBuzz = function () {
+	console.log("fizzBuzz!");
+}
+
 //setInterval( "alert('Hello')", 5000 );
 
 for (i=0; i <=numberPicked; i++) {
@@ -12,7 +40,7 @@ for (i=0; i <=numberPicked; i++) {
 
 
 //timer
-var timer;
+/*var timer;
 var totalSeconds;
 
 function createTimer(TimerID, Time) {
@@ -31,7 +59,7 @@ function tick() {
 
 function updateTimer() {
 	timer.innerHTML = totalSeconds;
-}
+}*/
 
 	/*if (i % 3 === 0 && i % 5 === 0) {
 		console.log("FizzBuzz!");
