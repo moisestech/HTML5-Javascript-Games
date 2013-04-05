9d536afa777d578cd5e7c11451971b756ad850c8
Moises404 <sesmoi.moises@live.com>
2013-04-04 20:37:01 -0400

    login all substring letters in array vocabList

---
+++ games/Hangman/hangman.js
@@ -0,0 +1,16 @@
+//global variables
+var vocabList = ["dragon", "super"];
+
+//log all words from the vocabList array
+for (i=0; i < vocabList.length; i++) {
+
+	var fullWord = vocabList[i];
+	var fullWordLength = fullWord.length;
+
+	for (j=0; j < fullWordLength; j++) {
+		console.log(fullWord[j]);
+	}
+
+	//console.log(vocabList[i]);
+	//console.log(i);
+}
