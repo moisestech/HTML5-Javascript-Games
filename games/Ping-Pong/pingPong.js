ab7fa01fa3503baed72e1e47bc6b31ca33a032d3
Moises404 <sesmoi.moises@live.com>
2013-04-06 00:40:59 -0400

    adding more css

--- games/Ping-Pong/pingPong.js
+++ games/Ping-Pong/pingPong.js
@@ -1,4 +1,22 @@
+//create jquery dom ready function
 $(function() {
+	//select paddleA and paddleB divs
 	$("#paddleB").css("top", "20px");
 	$("#paddleA").css("top", "60px");
+
+	//create KEY object
+	var KEY = {
+		UP: 38,
+		DOWN: 40,
+		W: 87,
+		S: 83
+	}
+
+	$(document).keydown(function(e) {
+		switch(e.which) {
+			case KEY.UP: //arrow-up
+			// get the current paddle B's top value in Int type
+			var top = parseInt($("#paddleB").css("top"));
+		}
+	})
 });
