a008d22c1e0451945f90de46fbdb46b39bea48e7
Moises404 <sesmoi.moises@live.com>
2013-04-07 10:13:33 -0400

    currently no errors in the move gameLoop

--- games/Ping-Pong/pingPong.js
+++ games/Ping-Pong/pingPong.js
@@ -6,9 +6,10 @@ var KEY = {
 	S: 83
 }
 
-// global variable array to store the key pressed status
-// inside the pingPong object
-var pingPong = {};
+// a global object to store all global variable we use for the game
+var pingPong = {}
+
+// an array to remember which key is pressed and which is not.
 pingPong.pressedKeys = [];
 
 // on DOM ready
@@ -18,45 +19,42 @@ $(function(){
 
 	// mark down what key is down and up into an array called
 	// "pressedKeys"
-
-	$(document).keydown(function(e) {
-		pingPong.pressedKeys[e.which] = true;
+	$(document).keydown(function(e){
+		pingpong.pressedKeys[e.which] = true;
+    });
+    $(document).keyup(function(e){
+    	pingpong.pressedKeys[e.which] = false;
 	});
-
-	$(document).keyup(function(e) {
-		pingPong.pressedKeys[e.which] = true;
-	});
-
-	function gameLoop() {
-		movePaddles();
-	}
-
-	function movePaddles() {
-		// use our custom timer to continuously check if a key
-		// is pressed.
-
-		if (pingPong.pressedKeys[KEY.UP]) {
-			// arrow-up, move the paddle B up 5 pixels
-			var top = parseInt($("#paddleB").css("top"));
-			$("#paddleB").css("top", top-5);
-		}
-
-		if (pingPong.pressedKeys[KEY.DOWN]) {
-			// arrow-down, move the paddle B down 5 pixels
-			var top = parseInt($("#paddleB").css("top"));
-			$("#paddleB").css("top", top+5);
-		}
-
-		if (pingPong.pressedKeys[KEY.W]) {
-			// w, move the paddle A up 5 pixels
-			var top = parseInt($("#paddleA").css("top"));
-			$("#paddleA").css("top", top-5);
-		}
-
-		if (pingPong.pressedKeys[KEY.S]) {
-			// s, move the paddle A down 5 pixels
-			var top = parseInt($("#paddleA").css("top"));
-			$("#paddleA").css("top", top+5);
-		}
-	}
 });
+
+function gameLoop() {
+		movePaddles();
+}
+
+function movePaddles() {
+	// use our custom timer to continuously check if a key
+	// is pressed.
+	if (pingPong.pressedKeys[KEY.UP]) {
+		// arrow-up, move the paddle B up 5 pixels
+		var top = parseInt($("#paddleB").css("top"));
+		$("#paddleB").css("top", top-5);
+	}
+
+	if (pingPong.pressedKeys[KEY.DOWN]) {
+		// arrow-down, move the paddle B down 5 pixels
+		var top = parseInt($("#paddleB").css("top"));
+		$("#paddleB").css("top", top+5);
+	}
+
+	if (pingPong.pressedKeys[KEY.W]) {
+		// w, move the paddle A up 5 pixels
+		var top = parseInt($("#paddleA").css("top"));
+		$("#paddleA").css("top", top-5);
+	}
+
+	if (pingPong.pressedKeys[KEY.S]) {
+		// s, move the paddle A down 5 pixels
+		var top = parseInt($("#paddleA").css("top"));
+		$("#paddleA").css("top", top+5);
+	}
+}
