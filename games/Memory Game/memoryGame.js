d3b1e7a1fddde8af9507a97e1232dab824ce32c9
Moises404 <sesmoi.moises@live.com>
2013-04-08 23:53:11 -0400

    adding js and fixing css

--- games/Memory Game/memoryGame.js
+++ games/Memory Game/memoryGame.js
@@ -1,4 +1,38 @@
-$(function() {
-	$("#card1").addClass("moveAndScale");
-	$("#card2").addClass("rotateRight");
+// a global object to hold all global variables related to the game.
+//var matchingGame = {};
+
+// all possible values for each card in deck
+/*matchingGame.deck = [
+	"cardAK", "cardAK",
+	"cardAQ", "cardAQ",
+	"cardAJ", "cardAJ",
+	"cardBK", "cardBK",
+	"cardBQ", "cardBQ",
+	"cardBJ", "cardBJ",
+];*/
+
+//every code inside $(function(){}) will be run
+// after the DOM is loaded and ready.
+//$(function() {
+	//shuffling the deck
+	//matchingGame.deck.sort(shuffle);
+
+	// clone 12 copies of the card
+	/*for(var i=0; i<11; i++) {
+		$(".card:first-child").clone().appendTo("#cards");
+	}
+
+
+})*/
+
+$(function(){
+	$("cards").children().each(function(index) {
+		//listen the click event on each card DIV element.
+		$(this).click(function(){
+			//add the class "card-flipped".
+			// the browser will animate the styles between current
+			//state and card-flipped state.
+			$(this).toggleClass("card-flipped")
+		});
+	});
 });
