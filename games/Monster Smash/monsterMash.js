36cf535aa27955836157e4e5f67505898aead0a0
Moises404 <sesmoi.moises@live.com>
2013-04-09 14:27:01 -0400

    learning to change the display state of an object and rendering the image in canvas

--- games/Monster Smash/monsterMash.js
+++ games/Monster Smash/monsterMash.js
@@ -1,20 +1,47 @@
 // Get a reference to the canvas
-var canvas = document.querySelector("canvas");
+//var canvas = document.querySelector("canvas");
 
 // Get a reference to the canvas's drawing surface
-var drawingSurface = canvas.getContext("2d");
+//var drawingSurface = canvas.getContext("2d");
 
 // Create a monsterImage variable to hold the new monsterImage
-var monsterImage = new Image();
+//var monsterImage = new Image();
 
 // Add a listener to the image to call the 
 // render function when the image has loaded
-monsterImage.addEventListener("load", loadHandler, false);
+//monsterImage.addEventListener("load", loadHandler, false);
 
 // Assign the image's src property to the image you want to load
-monsterImage.src = "imgs/monster.png";
+//monsterImage.src = "imgs/monster.png";
 
-function loadHandler() {
+/*function loadHandler() {
 	// Draw the monsterImage onto the drawingSurface
 	drawingSurface.drawImage(monsterImage, 0, 0);
+}*/
+
+
+var monster = {
+	//Define the monster's states
+	NORMAL: 0,
+	SCARED: 1,
+
+	//Set its initial state
+	state: 0
+};
+
+console.log("monster's initial state: " + monster.state);
+
+// Change the monster's state by pressing a key
+window.addEventListener("keydown", keydownHandler, false);
+
+function keydownHandler (event){
+	// Change the monster's state
+	monster.state = monster.SCARED;
+
+	//Display the new state
+	console.log("The monster's new state: " + monster.state);
 }
+
+
+
+
