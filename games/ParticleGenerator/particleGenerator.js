cbcdf07e78f926dedd62fe59036415825a3f1291
Moises404 <sesmoi.moises@live.com>
2013-04-14 08:38:46 -0400

    particle generator with random colors

--- games/ParticleGenerator/particleGenerator.js
+++ games/ParticleGenerator/particleGenerator.js
@@ -23,19 +23,25 @@ function randomPlusMinusY() {
 
 
 function draw() {
-	// Create a particle Object
+	
+	for (i=0; i<10; i++) {
+		// Create a particle Object
 	var particle = {
 		// store the X and y position in a varible
 		xPosition: 150,
 		yPosition: 150,
-		size: 20,
+		size: 10,
 
 		// store the speed of the square in a variable
-		xSpeed: Math.floor(Math.random()*10 + 5) * randomPlusMinusX(),
-		ySpeed: Math.floor(Math.random()*10 + 5) * randomPlusMinusY()
+		xSpeed: Math.random()*5 * randomPlusMinusX(),
+		ySpeed: Math.random()*5 * randomPlusMinusY()
+		}
+
+	particleArray.push(particle);	
 	}
 
-	particleArray.push(particle);
+	
+	console.log(particleArray.length);
 
 	// to create an animation the screen is cleared beofre reDraw
 	drawingSurface.clearRect(0, 0, 300, 300);
@@ -44,14 +50,19 @@ function draw() {
 	for (var i = 0; i<particleArray.length; i++) {
 		particle = particleArray[i];
 
-		drawingSurface.fillStyle = "red";
+		var Red = Math.round(Math.random()*255);
+		var Green = Math.round(Math.random()*255);
+		var Blue = Math.round(Math.random()*255);
+
+		drawingSurface.fillStyle = "rgb(" + Red + "," + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + ")";
 		drawingSurface.fillRect ( particle.xPosition, particle.yPosition, particle.size, particle.size);
 		// create a square on pixel over every time
 		particle.xPosition += particle.xSpeed;
 		particle.yPosition += particle.ySpeed;
 
 		// Shrink over time
-		particle.size = particle.size * 0.9;
+		particle.size = particle.size * 0.96;
+
 	}
 
 }
