5b9189f3317d0420952283c0fcb197e7c904d3ab
Moises404 <sesmoi.moises@live.com>
2013-05-17 23:00:05 -0400

    logging total score

--- games/FizzBuzz/fizzBuzz2.js
+++ games/FizzBuzz/fizzBuzz2.js
@@ -8,10 +8,13 @@
 var currentFizzBuzzNumber = 0;
 console.log(currentFizzBuzzNumber);
 
-// current FizzBuzz number
+//FizzBuzz individual count
 var fizzBuzzCount = 0;
 var fizzCount = 0;
 var buzzCount = 0;
+
+//FizzBuzz Total count
+var totalCount = 0;
 
 //DOM variables
 var currentNumber = document.getElementById("currentNumber");
@@ -24,35 +27,44 @@ function keydownHandler(event) {
 	switch (event.keyCode) {
 		case fizzBuzz:
 			if (currentFizzBuzzNumber % 3 === 0 && currentFizzBuzzNumber % 5 == 0) {
-			fizzBuzzCount++;
-			console.log("FizzBuzzNumber " + fizzBuzzCount);
+				fizzBuzzCount++;
+				//console.log("FizzBuzzNumber " + fizzBuzzCount);
+
+				totalCount+= 2;
+				console.log(totalCount);
 			}
 			
 			currentFizzBuzzNumber++;
 			currentNumber.innerHTML = currentFizzBuzzNumber;
-			console.log("The current number is " + currentFizzBuzzNumber);
+			//console.log("The current number is " + currentFizzBuzzNumber);
 			break;
 
 		case fizz:
 			if (currentFizzBuzzNumber % 3 === 0) {
 				fizzCount++;
-				console.log("FizzNumber " + fizzCount);
+				//console.log("FizzNumber " + fizzCount);
+				
+				totalCount++;
+				console.log(totalCount);
 			}
 			
 			currentFizzBuzzNumber++;
 			currentNumber.innerHTML = currentFizzBuzzNumber;
-			console.log("The current number is " + currentFizzBuzzNumber);
+			//console.log("The current number is " + currentFizzBuzzNumber);
 			break;
 
 		case buzz:
 			if (currentFizzBuzzNumber % 5 === 0) {
 				buzzCount++;
-				console.log("BuzzNumber " + buzzCount);
+				//console.log("BuzzNumber " + buzzCount);
+
+				totalCount++;
+				console.log(totalCount);
 			}
 
 			currentFizzBuzzNumber++;
 			currentNumber.innerHTML = currentFizzBuzzNumber;
-			console.log("The current number is " + currentFizzBuzzNumber);
+			//console.log("The current number is " + currentFizzBuzzNumber);
 			break;
 	} 
 }
