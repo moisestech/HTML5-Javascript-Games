22622881a6b3965ab45d7b556caeb260cd9ef961
Moises404 <sesmoi.moises@live.com>
2013-05-28 23:34:52 -0400

    width and height variables are created randomly every three seconds

--- games/Pixel Hero/pixelHero.js
+++ games/Pixel Hero/pixelHero.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	console.log($("#gameSquare").css("width"));
+	//console.log($("#gameSquare").css("width"));
 	console.log("ready!");
 
 	var widthInput = $("#widthInput");
@@ -10,6 +10,23 @@ $(document).ready(function() {
 		alert(widthInput.val() + heightInput.val());
 		return false;
 	});
+
+	var htmlBody = $("body");
+	htmlBody.append("<div id='gameSquare'>Here you go</div>");
+
+	var gameSquare = $("#gameSquare");
+
+	setInterval(createRandomMeasurements, 3000);
+
+		function createRandomMeasurements() {
+			var widthPixel = Math.floor(Math.random()*200);
+			var heightPixel = Math.floor(Math.random()*200);
+
+			console.log("Width: " + widthPixel +  ", Height: " + heightPixel);
+		}
+		
+
+
 
 });
 
